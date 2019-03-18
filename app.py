29db9556892ee9482c69086a90b8a07d7286e4f4
krec-kiran <krec.kiran@gmail.com>
2019-03-18 05:15:58 +0000

    REST end points implementation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -27,33 +27,55 @@ def hello_world():
 # POST /store/data:{name}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
     # print('Sainsburys!')
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+        return jsonify({'message:store not found'})
 
 
 # Pull request TEST
 # GET /store
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
 
 
 # POST /store/<string:name>/item:{name:,price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store():
-    pass
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['items']['name'],
+                'price': request_data['items']['price']
+
+            }
+            store['items'].append(new_item)
+        return jsonify(store)
+    return jsonify({'message:item not found in store'})
 
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    pass
+def get_items_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+        return jsonify({'message:store not found'})
 
 
 if __name__ == '__main__':
